c3594f22e9a22e75bbd40a635f3a282f5826eac6
Ryucel <rustuyucel@protonmail.com>
2025-04-02 11:34:01 +0300

    ConfigUpdate

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/TUFE_hesaplama_v2/',
   plugins: [
     react(),
     VitePWA({
@@ -14,12 +15,12 @@ export default defineConfig({
         theme_color: '#3182CE',
         icons: [
           {
-            src: '/icon-192x192.png',
+            src: '/TUFE_hesaplama_v2/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icon-512x512.png',
+            src: '/TUFE_hesaplama_v2/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
